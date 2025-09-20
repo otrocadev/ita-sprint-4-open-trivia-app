aeff0638ded61d6c7ac0cf92451c17d05241ea9c
Ot Roca <otrocadev@gmail.com>
2025-09-20 21:14:33 +0200

    feat: add browser geolocation support check before getting user location

--- src/services/getLocation.ts
+++ src/services/getLocation.ts
@@ -4,6 +4,9 @@ export const getLocation = (): Promise<{
 }> => {
   return new Promise((resolve, reject) => {
     try {
+      if (!navigator.geolocation) {
+        throw new Error('Geolocation is not supported by this browser')
+      }
       navigator.geolocation.getCurrentPosition((position) => {
         resolve({
           latitude: position.coords.latitude,
