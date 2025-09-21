570c2ccc6ac6881500e27aed0dacfb16b4970451
Ot Roca <otrocadev@gmail.com>
2025-09-21 20:08:03 +0200

    feat: add weather info display with dynamic icons in footer

--- src/services/wheatherAPI.ts
+++ src/services/wheatherAPI.ts
@@ -2,15 +2,26 @@ import { fetchAPI } from './fetchAPI'
 import { getLocation } from './getLocation'
 
 const OPEN_METEO_API_BASE_URL = import.meta.env.PUBLIC_OPEN_METEO_API
+const params = 'current=is_day,rain,temperature_2m,snowfall'
 
-export const getCurrentWheader = async () => {
+export const getCurrentWeather = async () => {
   try {
     const { latitude, longitude } = await getLocation()
-    const params = 'current=is_day,rain,temperature_2m,snowfall'
     const currentWeatherEndpoint = `${OPEN_METEO_API_BASE_URL}?latitude=${latitude}&longitude=${longitude}&${params}`
     const currentWeatherData = await fetchAPI(currentWeatherEndpoint)
-    console.log('Current weather data:', currentWeatherData)
+    return formatWeatherInfo(currentWeatherData)
   } catch (error) {
     console.error(error)
+    throw error
   }
+}
+
+export const formatWeatherInfo = (weatherInfo: any) => {
+  const wheatherInfo = {
+    temperature: weatherInfo.current.temperature_2m,
+    isDay: weatherInfo.current.is_day,
+    rain: weatherInfo.current.rain.toFixed(2),
+    snowfall: weatherInfo.current.snowfall.toFixed(2),
+  }
+  return wheatherInfo
 }
